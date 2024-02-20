e4d6bc41611f71674dc702bb49ffad5bb8caf43e
jinwoo Lee <jinwoolee@jinwooui-noteubug.local>
2024-02-20 09:39:46 +0900

    [REFACTOR] MemberAuthAdapterImpl Check Style 적용

--- src/main/java/store/ckin/auth/member/adapter/impl/MemberAuthAdapterImpl.java
+++ src/main/java/store/ckin/auth/member/adapter/impl/MemberAuthAdapterImpl.java
@@ -1,16 +1,19 @@
 package store.ckin.auth.member.adapter.impl;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.*;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import store.ckin.auth.config.ServerPortProperties;
 import store.ckin.auth.member.adapter.MemberAuthAdapter;
 import store.ckin.auth.member.dto.LoginRequestDto;
 import store.ckin.auth.member.dto.LoginResponseDto;
-
-import java.util.List;
 
 /**
  * MemberAuthAdapter 의 구현체 입니다.
