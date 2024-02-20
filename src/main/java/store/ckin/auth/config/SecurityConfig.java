21ebfb14f6e0a76e2655fc81904af4b173d27f4a
jinwoo Lee <jinwoolee@jinwooui-noteubug.local>
2024-02-20 10:58:04 +0900

    [FEAT] SecurityConfig

---
+++ src/main/java/store/ckin/auth/config/SecurityConfig.java
@@ -0,0 +1,40 @@
+package store.ckin.auth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * Security 설정을 위한 클래스 입니다.
+ *
+ * @author : jinwoolee
+ * @version : 2024. 02. 20.
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+
+    /**
+     * Security Filter 를 설정하는 Bean method 입니다.
+     *
+     * @param http the http
+     * @return the security filter chain
+     */
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .httpBasic().disable()
+                .formLogin().disable()
+                .logout().disable();
+
+        return http.build();
+    }
+}
