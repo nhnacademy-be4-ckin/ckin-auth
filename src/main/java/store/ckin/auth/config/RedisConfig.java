f876b82782b49ef5fcfd4c6905ffa55b3c8e4ca9
jinwoo Lee <jinwoolee@jinwooui-noteubug.local>
2024-02-23 09:51:24 +0900

    [FEAT] RedisConfig

---
+++ src/main/java/store/ckin/auth/config/RedisConfig.java
@@ -0,0 +1,52 @@
+package store.ckin.auth.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+/**
+ * Redis 설정을 위한 클래스 입니다.
+ *
+ * @author : jinwoolee
+ * @version : 2024. 02. 23.
+ */
+@Configuration
+public class RedisConfig {
+    @Value("${spring.data.redis.host}")
+    private String host;
+
+    @Value("${spring.data.redis.port}")
+    private int port;
+
+    /**
+     * Redis connection factory redis connection factory.
+     *
+     * @return the redis connection factory
+     */
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setHostName(host);
+        config.setPort(port);
+
+        return new LettuceConnectionFactory(config);
+    }
+
+    /**
+     * JWT 관리를 위한 Redis Template 를 Bean 으로 등록한 클래스 입니다.
+     *
+     * @return RedisTemplate
+     */
+    @Bean
+    public RedisTemplate<?, ?> redisTemplate() {
+        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+
+        return redisTemplate;
+    }
+
+}
